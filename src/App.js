8f7b5f77f78f98bcb19d68981da600110988e457
islamalsawaftah <islamalsawaftah99@gmail.com>
2022-03-02 11:02:18 +0300

    add tours to home file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import Home from "./components/home/Home";
-import Tours from "./components/tours/Tours";
-import data from "./data/data.json";
 
 function App() {
   // console.log(data)
   return (
     <>
       <Home />
-      <Tours datas={data} />
     </>
   )
 }
